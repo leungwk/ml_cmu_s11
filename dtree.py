4e979c9f2a884343f54c291409351a7160236ffb
William Leung <wleung@wlcomp>
2014-09-09 18:34:36 -0400

    Added pruning top-down and bottom-up.
    Added notes on decision tree implementation, effect of pruning type, and stats given variations in \epsilon.

--- dtree.py
+++ dtree.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from scipy.stats import entropy
+import copy
 import numpy as np
+import time as pytime
 
 SEP_SSV=' '
 
 class Node(object):
+    """Nodes used to construct the tree. Stores the attribute and value splitted upon (from the parent), and the predicted label"""
     def __init__(self, children=None, parent=None, data=None, attribute=None, attribute_value=None, label=None):
         self.children = children if children is not None else []
         self.parent = parent # for going up
@@ -21,6 +24,21 @@ def print_tree(root, attr_target):
 
     return [root.attribute, root.label, root.attribute_value, _vals(root.data, attr_target), [print_tree(child, attr_target) for child in root.children]]
 
+
+def tree_stats(root):
+    """Starting at root, return the number of nodes in the tree and its depth"""
+    def _ts(tree, depth):
+        if not tree.children: # is a leaf
+            return 1, depth
+
+        n_node = 1
+        max_depth = depth
+        for child in tree.children:
+            nn, ret_d = _ts(child, depth+1)
+            max_depth = max(max_depth, ret_d)
+            n_node += nn
+        return n_node, max_depth
+    return _ts(root, 0)
 
 
 def parse_input(filepath):
@@ -63,6 +81,7 @@ def arg_and_max(arg_vals):
 
 
 def build_tree(df_train, attr_tests, attr_target):
+    """ID3"""
     label_counts = df_train[attr_target].value_counts()
     best_label = label_counts.argmax()
     label_counts = label_counts.to_dict() # .to_dict() because of a bug with Index in pandas 0.14.1 (compared to Int64Index)
@@ -90,39 +109,143 @@ def build_tree(df_train, attr_tests, attr_target):
             leaf = Node(parent=root, attribute_value=val)
             root.children.append(leaf)
         else:
-            child = build_tree(df_attr_val_sel, [x for x in df_attr_val_sel.columns if x not in [attr_target,best_attr]], attr_target)
+            child = build_tree(
+                df_attr_val_sel,
+                [x for x in df_attr_val_sel.columns if x not in [attr_target,best_attr]], # select the remaining attributes (ie. use each attribute at most once)
+                 attr_target)
             child.attribute_value = val
             root.children.append(child)
     return root
 
 
-def classification_accuracy(tree, df_train_in, df_test_in, attr_target):
-    def _ca(tree, df_train, df_test):
+def classification_accuracy(root, df_train_in, df_test_in, attr_target):
+    """calculate classification accuracy for train and test data at the same time so to traverse only once"""
+    def _bin_count(counts):
+        return counts.get('0', counts.get(0, 0)), counts.get('1', counts.get(1, 0))        
+
+    def _ca(tree, df_train, df_test, depth):
         if not tree.children: # is a leaf
-            label_train = df_train[attr_target].value_counts().argmax()
-            label_test = df_test[attr_target].value_counts().argmax()
-            return [[tree.label, label_train, label_test]]
+            tmp_ser_train_vals = df_train[attr_target].value_counts()
+            tmp_ser_test_vals = df_test[attr_target].value_counts()
+            label_train, label_test = None, None
+            val_0_train, val_1_train = 0,0
+            val_0_test, val_1_test = 0,0
+            if not tmp_ser_train_vals.empty:
+                label_train = tmp_ser_train_vals.argmax()
+                val_0_train, val_1_train = _bin_count(tmp_ser_train_vals.to_dict()) # count the exact number
+            if not tmp_ser_test_vals.empty:
+                label_test = tmp_ser_test_vals.argmax()
+                val_0_test, val_1_test = _bin_count(tmp_ser_test_vals.to_dict())
+
+            return [[tree.label, label_train, label_test, np.array([val_0_train, val_1_train]), np.array([val_0_test, val_1_test])]], [depth, 1]
 
         ## not a leaf
         acc = []
+        max_depth = 0
+        n_node = 1 # for the inner node
         for child in tree.children:
             attr = tree.attribute
             attr_val = child.attribute_value
-            res = _ca(child, df_train[df_train[attr] == attr_val], df_test[df_test[attr] == attr_val])
+            res, (res_depth, cnt_child) = _ca(child, df_train[df_train[attr] == attr_val], df_test[df_test[attr] == attr_val], depth+1)
+            max_depth = max(max_depth, res_depth)
+            n_node += cnt_child
             acc.extend(res) # to flatten the tree output
-        return acc
+        return acc, [max_depth, n_node]
 
-    res = _ca(tree, df_train_in, df_test_in)
-    mtx_train = pd.DataFrame(np.zeros((2,2)), columns=['1','0'], index=['1','0'])
-    mtx_train.index.name = 'out'
-    mtx_test = mtx_train.copy()
+    res, (res_depth, n_node) = _ca(root, df_train_in, df_test_in, 0)
+    mtx_lab_train = pd.DataFrame(np.zeros((2,2)), columns=['1','0'], index=['1','0'])
+    mtx_lab_train.index.name = 'out' # output value, not the base label
+    mtx_lab_test = mtx_lab_train.copy()
+
+    mtx_cnt_train, mtx_cnt_test = mtx_lab_train.copy(), mtx_lab_train.copy()
     def _sel(label_tree, label):
-        return (str(label_train) if label_tree == label else '0'), ('1' if label_tree in [1,'1'] else '0')
-    for label_tree, label_train, label_test in res:
-        mtx_train.loc[_sel(label_tree, label_train)] += 1
-        mtx_test.loc[_sel(label_tree, label_test)] += 1
+        return (str(label) if str(label_tree) == str(label) else '0'), str(label_tree)
+    for label_tree, label_train, label_test, cnt_train, cnt_test in res:
+        ## count the predicted label
+        mtx_lab_train.loc[_sel(label_tree, label_train)] += 1
+        mtx_lab_test.loc[_sel(label_tree, label_test)] += 1
 
-    return res, mtx_train, mtx_test
+        ## count per example
+        mtx_cnt_train.loc[str(label_tree), '0'] += cnt_train[0]
+        mtx_cnt_train.loc[str(label_tree), '1'] += cnt_train[1]
+
+        mtx_cnt_test.loc[str(label_tree), '0'] += cnt_test[0]
+        mtx_cnt_test.loc[str(label_tree), '1'] += cnt_test[1]
+
+    return res, mtx_lab_train, mtx_lab_test, mtx_cnt_train, mtx_cnt_test
+
+
+def _ca(df_mtx):
+    denom = long(df_mtx.sum().sum())
+    ac = 1.*(df_mtx.loc['1','1'] +df_mtx.loc['0','0'])/denom if denom > 0 else None
+    return ac
+
+
+def prune_top_down(tree_in, df_train_in, df_valid_in, attr_target, epsilon=0.005):
+    """Prune by comparing the pre- and post- prune validation accuracy"""
+    root = copy.deepcopy(tree_in) # classification_accuracy() expects the root, even if nodes below have been pruned; use deepcopy to ease destructive updates
+    acc_stats = []
+    def _prune(tree, branch_num, depth):
+        if not tree.children: # is a leaf
+            ## don't check and return
+            return
+
+        ## depth-first traversal (check before recur); destructive
+        ## before pruning
+        _, _, _, _, df_valid_old = classification_accuracy(root, df_train_in, df_valid_in, attr_target) # use "root" because classification_accuracy() starts from the top and might visit every branch; that df_train_in is expected should not add too much unneeded overhead even though its results are not used
+        ## after pruning
+        tmp_els = tree.children
+        tree.children = []
+        _, _, _, _, df_valid = classification_accuracy(root, df_train_in, df_valid_in, attr_target)
+        
+        ac_old, ac = _ca(df_valid_old), _ca(df_valid)
+        acc_stats.append( (ac_old, ac, branch_num, depth) )
+        if ac -ac_old >= epsilon:
+            pass # ie. prune
+        else:
+            tree.children = tmp_els # add back values
+            for idx, child in enumerate(tree.children): # recur
+                _prune(child, idx, depth+1)
+    _prune(root, 0, 0)
+    return root, pd.DataFrame(acc_stats, columns=['ac_b','ac_a','cidx','depth'])
+
+
+def prune_bottom_up(tree_in, df_train_in, df_valid_in, attr_target, epsilon=0.005):
+    """Prune by comparing the pre- and post- prune validation accuracy"""
+    if not tree_in.children: # is a stump
+        return tree_in, pd.DataFrame([])
+
+    root = copy.deepcopy(tree_in) # see prune_top_down
+    acc_stats = []
+    def _prune(tree, branch_num, depth):
+        idx = 0
+        while True:
+            try:
+                child = tree.children[idx]
+            except IndexError:
+                break # end of list
+
+            if not child.children: # child is a leaf, so check as if one recurred
+                ## both classification_accuracy() s take up all the time
+                ## before
+                _, _, _, _, df_valid_old = classification_accuracy(root, df_train_in, df_valid_in, attr_target)
+                ## after
+                del tree.children[idx]
+                _, _, _, _, df_valid = classification_accuracy(root, df_train_in, df_valid_in, attr_target)
+                
+                ac_old, ac = _ca(df_valid_old), _ca(df_valid)
+                acc_stats.append( (ac_old, ac, branch_num, depth) )
+                if ac -ac_old >= epsilon:
+                    pass # ie. prune (don't add child back)
+                    ## idx now at the next item after `child'
+                else: # add child back
+                    tree.children.insert(idx, child)
+                    idx += 1 # move to the next item
+            else: # recur
+                _prune(child, idx, depth+1) # could potentially remove all children of this child
+                idx += 1
+    _prune(root, 0, 0)
+    return root, pd.DataFrame(acc_stats, columns=['ac_b','ac_a','cidx','depth'])
 
 
 if __name__ == '__main__':
@@ -139,4 +262,49 @@ if __name__ == '__main__':
 
     attr_target = 'poisonous'
     attr_tests = [x for x in df_train.columns if x != attr_target]
+
     tree = build_tree(df_train, attr_tests, attr_target)
+    _, _, _, mtx_cnt_train, mtx_cnt_test = classification_accuracy(tree, df_train, df_test, attr_target=attr_target)
+
+    ## training and testing
+    ## accuracy without pruning
+    _ca(mtx_cnt_train), _ca(mtx_cnt_test)
+    tree_stats(tree)
+
+
+    ## experiment with different \epsilons
+    acc_stats_top = []
+    acc_stats_bottom = []
+    epsilons = [0.001,0.002,0.003,0.004,0.006,0.008,0.009,0.010,0.012,0.014,0.015,0.016,0.020,0.024,0.028,0.032,0.064]
+    for epsilon in epsilons:
+
+        start_time = pytime.time()
+        tree_prune_bottom, df_stats_bottom_prune = prune_bottom_up(tree, df_train, df_valid, attr_target, epsilon=epsilon)
+        _, _, _, mtx_cnt_train_bottom_prune, mtx_cnt_test_bottom_prune = classification_accuracy(tree_prune_bottom, df_train, df_test, attr_target=attr_target)
+        ca_bottom_prune_train, ca_bottom_prune_test = _ca(mtx_cnt_train_bottom_prune), _ca(mtx_cnt_test_bottom_prune)
+        n_node, depth = tree_stats(tree_prune_bottom)
+        end_time = pytime.time()
+
+        row = (epsilon, ca_bottom_prune_train, ca_bottom_prune_test, n_node, depth, end_time -start_time)
+        acc_stats_bottom.append( row )
+        print ('bottom', row)
+
+
+        start_time = pytime.time()
+        tree_prune_top, df_stats_top_prune = prune_top_down(tree, df_train, df_valid, attr_target, epsilon=epsilon)
+        _, _, _, mtx_cnt_train_top_prune, mtx_cnt_test_top_prune = classification_accuracy(tree_prune_top, df_train, df_test, attr_target=attr_target)
+        # training and testing
+        # accuracy with top-down pruning
+        ca_prune_train, ca_prune_test = _ca(mtx_cnt_train_top_prune), _ca(mtx_cnt_test_top_prune)
+        n_node, depth = tree_stats(tree_prune_top)
+        end_time = pytime.time()
+
+        row = (epsilon, ca_prune_train, ca_prune_test, n_node, depth, end_time -start_time)
+        acc_stats_top.append( row )
+        print ('top', row)
+
+    df_prune_top_stats_tot = pd.DataFrame(acc_stats_top, columns=['e','cap_train','cap_test','n','d','t'])
+    df_prune_bottom_stats_tot = pd.DataFrame(acc_stats_bottom, columns=['e','cap_train','cap_test','n','d','t'])
+
+    print df_prune_top_stats_tot
+    print df_prune_bottom_stats_tot
--- tests/dtree.py
+++ tests/dtree.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from ..dtree import build_tree, parse_input, classification_accuracy
+from ..dtree import build_tree, parse_input, classification_accuracy, tree_stats, prune_top_down, prune_bottom_up
 
 df_train = parse_input('data/hw1/noisy10_train.ssv')
 df_test = parse_input('data/hw1/noisy10_test.ssv')
@@ -63,6 +63,13 @@ df_samp4 = pd.DataFrame(acc, columns=['y','x1','x2'])
 tree4 = build_tree(df_samp4, ['x1','x2'], 'y')
 # TODO: output an attribute?
 
+_, _, _, _, _ = classification_accuracy(tree4, df_samp4, df_samp4, attr_target='y')
+n_node, depth = tree_stats(tree4)
+assert depth == 0
+assert n_node == 1
+
+
+
 # acc = []
 # df_samp5 = pd.DataFrame()
 # tree5 = build_tree(df_samp5, [], '') # doesn't handle this
@@ -85,7 +92,7 @@ tree6 = build_tree(df_samp6, ['x1','x2','x3'], 'y')
 acc_test = [
     (0,'a','m','x'),
     (0,'a','m','x'),
-    (0,'b','m','y'),
+    (0,'b','m','y'), # only this changed (1 \to 0)
     (1,'b','m','y'),
     (1,'b','n','x'),
     (1,'b','n','x'),
@@ -97,4 +104,28 @@ df_samp6_test = pd.DataFrame(acc_test, columns=['y','x1','x2','x3'])
 # tree6_test = build_tree(df_samp6_test, ['x1','x2','x3'], 'y')
 # print_tree(tree6, attr_target='y')
 
-classification_accuracy(tree6, df_samp6, df_samp6_test, attr_target='y')
+_, _, _, _, _ = classification_accuracy(tree6, df_samp6, df_samp6_test, attr_target='y')
+n_node, depth = tree_stats(tree6)
+assert depth == 3
+assert n_node == 7
+
+tree6_prune_top, df_stats_6_top = prune_top_down(tree6, df_samp6, df_samp6_test, attr_target='y', epsilon=0.005)
+tree6_prune_bottom, df_stats_6_bottom = prune_bottom_up(tree6, df_samp6, df_samp6_test, attr_target='y', epsilon=0.005)
+
+_, _, _, _, _ = classification_accuracy(tree6_prune_top, df_samp6, df_samp6_test, attr_target='y')
+_, _, _, _, _ = classification_accuracy(tree6_prune_bottom, df_samp6, df_samp6_test, attr_target='y')
+    # (0,'b','m','y'), # only this changed (1 \to 0)
+    # (1,'b','m','y'),
+# TODO: there shouldn't be 100% test accuracy...
+# classification_accuracy() ignores where there is no value? (should it be doing this?, or should it classify irregardless ...)
+# because in the bottom prune, (b,m,*) is missing. It should go per entry in data, and if there are no further children, use the current node. This will handle the case where the tree is not complete over the input space (ie. there is no node for the entire x_1,\dots,x_n sequence), and will allow for only partial sequences.
+
+
+
+import copy
+root = copy.deepcopy(tree6)
+acc_stats = []
+epsilon = 0.005
+attr_target='y'
+from dtree import __prune
+# %lprun -f __prune __prune(tree6, 0, 0, root, df_samp6, df_samp6_test, attr_target, epsilon, acc_stats)
